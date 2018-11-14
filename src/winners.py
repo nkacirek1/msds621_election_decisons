ba795bb8377534abc5aadd7afd5b3b0956f9e399
Nicole Kacirek <nicolekacirek@Nicoles-MacBook-Pro.local>
2018-11-13 18:37:46 -0800

    added GA districts with declared winners. Some are still not declared and two ran unopposed

--- src/winners.py
+++ src/winners.py
@@ -1,4 +1,7 @@
 won = {
+    'GA_D1': 'R', 'GA_D2': 'D', 'GA_D3': 'R', 'GA_D4': 'D',
+    'GA_D6': 'D', 'GA_D9': 'R', 'GA_D10': 'R', 'GA_D11': 'R',
+    'GA_D12': 'R', 'GA_D13': 'D', 'GA_D14': 'R',
     'MN_D1': 'R', 'MN_D2': 'D', 'MN_D3': 'D', 'MN_D4': 'D', 'MN_D5': 'D',
     'MN_D6': 'R', 'MN_D7': 'D', 'MN_D8': 'R', 'MN_SEN': 'D',
     'NC_D1': 'D', 'NC_D2': 'R', 'NC_D3': 'R', 'NC_D4': 'D', 'NC_D5': 'D',
